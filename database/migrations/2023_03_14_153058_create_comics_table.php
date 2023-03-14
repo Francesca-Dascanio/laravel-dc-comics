2af4db1f1cbb362f08ba82b114974eb766889ac2
“Francesca <dascanio.francesca@gmail.com>
2023-03-14 16:40:35 +0100

    Milestone 1 > completata

---
+++ database/migrations/2023_03_14_153058_create_comics_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+// Models
+use App\Models\Comic;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title', 50);
+            $table->text('description');
+            $table->string('thumb', 500);
+            $table->unsignedFloat('price', 6, 2);
+            $table->string('series', 50);
+            $table->date('sale_date');
+            $table->string('type', 50);
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
