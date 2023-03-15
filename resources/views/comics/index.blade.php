3dfeca536aeaad459f08ba95da37473cf0fd3d53
“Francesca <dascanio.francesca@gmail.com>
2023-03-15 15:13:08 +0100

    Consegna 2> bonus completato

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -97,9 +97,34 @@
 
                                         @method('DELETE')
 
-                                            <button type="submit" class="btn btn-danger">
+                                            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
                                                 Delete
                                             </button>
+
+                                            <!-- Modal -->
+                                            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                <div class="modal-dialog">
+                                                <div class="modal-content">
+                                                    <div class="modal-header">
+                                                    <h1 class="modal-title fs-5" id="exampleModalLabel">
+                                                        Delete comic
+                                                    </h1>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        Are you sure to DELETE this comic?
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                                                        No, come back
+                                                    </button>
+                                                    <button type="submit" class="btn btn-danger">
+                                                        Yes, delete the comic
+                                                    </button>
+                                                    </div>
+                                                </div>
+                                                </div>
+                                            </div>
                                         </form>
                                     </td>
                                 </tr>  
